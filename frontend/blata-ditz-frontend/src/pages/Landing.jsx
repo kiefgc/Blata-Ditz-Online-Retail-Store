b236ef28b88b7a8ab61f430aae5b69507eaed440
julianasj <juliana.jimeno@ciit.edu.ph>
2025-11-11 16:49:49 +0800

    Navigation Bar. Set font-size root em to 62.5%

--- frontend/blata-ditz-frontend/src/pages/Landing.jsx
+++ frontend/blata-ditz-frontend/src/pages/Landing.jsx
@@ -2,9 +2,46 @@ import "./Landing.css";
 
 function Landing() {
   return (
-    <div className="landing-container">
-      <h1>Welcome to Blata Ditz</h1>
-    </div>
+    <>
+      <div class="navbar">
+        <div>
+          <a href="#" class="logo">
+            BLATADITZ
+          </a>
+        </div>
+        <div>
+          <a href="#">
+            <div class="search-bar">
+              <img
+                width="20"
+                height="20"
+                src="https://img.icons8.com/ios-glyphs/30/FFD033/search--v1.png"
+                alt="search--v1"
+              />
+              <input type="text" placeholder="Search" />
+            </div>
+          </a>
+        </div>
+        <div class="nav-links">
+          <a href="#">
+            <img
+              width="34"
+              height="34"
+              src="https://img.icons8.com/sf-regular/48/FFFFFF/shopping-cart.png"
+              alt="shopping-cart"
+            />
+          </a>
+          <a href="#">
+            <img
+              width="32"
+              height="32"
+              src="https://img.icons8.com/fluency-systems-regular/48/FFFFFF/user-male-circle--v1.png"
+              alt="user-male-circle--v1"
+            />
+          </a>
+        </div>
+      </div>
+    </>
   );
 }
 
