fc1b18343cf90e092e37c1cbaf8d90eeb19f4743
julianasj <juliana.jimeno@ciit.edu.ph>
2025-11-21 01:12:46 +0800

    Updates on Orders Tab Admin View

--- frontend/blata-ditz-frontend/src/pages/AdminOrders.jsx
+++ frontend/blata-ditz-frontend/src/pages/AdminOrders.jsx
@@ -199,6 +199,255 @@ function AdminOrders() {
               </li>
             </ul>
           </div>
+          <div className="main-content">
+            {/* ORDERS SECTION */}
+
+            <div className="admin-orders-section">
+              <div className="search-bar">
+                <img
+                  width="20"
+                  height="20"
+                  src="https://img.icons8.com/ios-glyphs/30/FFD033/search--v1.png"
+                  alt="search--v1"
+                />
+                <input type="text" placeholder="Search orders" />
+              </div>
+              <div className="admin-orders-tab">
+                <ul>
+                  <li>All</li>
+                  <li>To Pay</li>
+                  <li>To Ship</li>
+                  <li>To Receive</li>
+                  <li>Completed</li>
+                  <li>Cancelled</li>
+                </ul>
+              </div>
+
+              <div className="admin-orders-columnHeader">
+                <span>Order ID</span>
+                <span>Date</span>
+                <span>Total Amount</span>
+                <span>Payment Status</span>
+                <span>Order Status</span>
+              </div>
+              <div
+                className="admin-orders-list"
+                onClick={() => setShowOrdersPopup(true)}
+              >
+                <span>001</span>
+
+                <span>₱16,450.00</span>
+                <span>06-11-2025</span>
+                <span>
+                  <div className=" details-status-payment details-status-payment-table">
+                    <select
+                      name="details-status-payment"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                      }}
+                    >
+                      <option value="" disabled hidden></option>
+                      <option value="payment-pending">PENDING</option>
+                      <option value="paid">PAID</option>
+                      <option value="failed">FAILED</option>
+                    </select>
+                  </div>
+                </span>
+                <span>
+                  <div className=" details-status-payment details-status-payment-table">
+                    <select
+                      name="details-status-payment"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                      }}
+                    >
+                      <option value="" disabled hidden></option>
+                      <option value="payment-pending">PENDING</option>
+                      <option value="paid">PAID</option>
+                      <option value="failed">FAILED</option>
+                    </select>
+                  </div>
+                </span>
+              </div>
+              <div className="admin-orders-list">
+                <span>001</span>
+
+                <span>₱16,450.00</span>
+                <span>06-11-2025</span>
+                <span>
+                  <div className=" details-status-payment details-status-payment-table">
+                    <select
+                      name="details-status-payment"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                      }}
+                    >
+                      <option value="" disabled hidden></option>
+                      <option value="payment-pending">PENDING</option>
+                      <option value="paid">PAID</option>
+                      <option value="failed">FAILED</option>
+                    </select>
+                  </div>
+                </span>
+                <span>
+                  <div className=" details-status-payment details-status-payment-table">
+                    <select
+                      name="details-status-payment"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                      }}
+                    >
+                      <option value="" disabled hidden></option>
+                      <option value="payment-pending">PENDING</option>
+                      <option value="paid">PAID</option>
+                      <option value="failed">FAILED</option>
+                    </select>
+                  </div>
+                </span>
+              </div>
+              <div className="admin-orders-list">
+                <span>001</span>
+
+                <span>₱16,450.00</span>
+                <span>06-11-2025</span>
+                <span>
+                  <div className=" details-status-payment details-status-payment-table">
+                    <select
+                      name="details-status-payment"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                      }}
+                    >
+                      <option value="" disabled hidden></option>
+                      <option value="payment-pending">PENDING</option>
+                      <option value="paid">PAID</option>
+                      <option value="failed">FAILED</option>
+                    </select>
+                  </div>
+                </span>
+                <span>
+                  <div className=" details-status-payment details-status-payment-table">
+                    <select
+                      name="details-status-payment"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                      }}
+                    >
+                      <option value="" disabled hidden></option>
+                      <option value="payment-pending">PENDING</option>
+                      <option value="paid">PAID</option>
+                      <option value="failed">FAILED</option>
+                    </select>
+                  </div>
+                </span>
+              </div>
+            </div>
+
+            {/* Orders Popup */}
+            {showOrdersPopup && (
+              <div className="admin-orders-popup-overlay">
+                <div className="admin-orders-popup">
+                  <div
+                    className="admin-orders-popup-close"
+                    onClick={() => setShowOrdersPopup(false)}
+                  >
+                    CLOSE
+                  </div>
+                  <div className="admin-orders-popup-content">
+                    <div className="orders-id">
+                      <span className="order-id-title">ID No. </span>
+                      <span className="order-id-no">001</span>
+                    </div>
+                    <div className="order-columns">
+                      <div className="first-column">
+                        <div className="order-details">
+                          <span className="order-detail-title">Date</span>
+                          <span className="order-detail-value">09-23-25</span>
+                        </div>
+                        <div className="order-details">
+                          <span className="order-detail-title">
+                            Customer Contact
+                          </span>
+                          <span className="order-detail-value">
+                            <span>dana.alania@fmail.com</span>
+                            <br />
+                            <span>+63 976 348 5930</span>
+                          </span>
+                        </div>
+                        <div className="order-details">
+                          <span className="order-detail-title">
+                            Shipping Address
+                          </span>
+                          <span className="order-detail-value">
+                            578 Eymard Sweets, 1354, Quezon City, Metro Manila
+                          </span>
+                        </div>
+                      </div>
+                      <div className="second-column">
+                        <div className="order-details">
+                          <span className="order-detail-title">
+                            Payment Method
+                          </span>
+                          <span className="order-detail-value">GCash</span>
+                        </div>
+                        <div className="order-details">
+                          <div className="order-details-status">
+                            <div className=" details-status-payment">
+                              <span className="order-detail-title">
+                                Payment Status
+                              </span>
+                              <select name="details-status-payment">
+                                <option value="" disabled hidden></option>
+                                <option value="payment-pending">PENDING</option>
+                                <option value="paid">PAID</option>
+                                <option value="failed">FAILED</option>
+                              </select>
+                            </div>
+
+                            <div className=" details-status-order">
+                              <span className="order-detail-title">
+                                Order Status
+                              </span>
+                              <select name="details-status-order">
+                                <option value="" disabled hidden></option>
+                                <option value="order-pending">PENDING</option>
+                                <option value="processing">PROCESSING</option>
+                                <option value="completed">COMPLETED</option>
+                                <option value="cancelled">CANCELLED</option>
+                              </select>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="order-details">
+                      <span className="order-detail-title">Order Details</span>
+
+                      <div className="order-details-columnHeader">
+                        <span>ID</span>
+                        <span></span>
+                        <span>Name</span>
+                        <span></span>
+                        <span>Amount</span>
+                      </div>
+                      <div className="order-details-list">
+                        <span>001</span>
+                        <span>x2</span>
+                        <span>
+                          <div className="order-details-img">
+                            <img />
+                          </div>
+                        </span>
+                        <span>Name</span>
+                        <span>₱16,450.00</span>
+                      </div>
+                    </div>
+                    <div className="order-details"></div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </>
