97cc8b1dff845fb98212be91c969ed87c80278c1
kiefgc <tyrell.chua@ciit.edu.ph>
2025-11-21 00:48:32 +0800

    Fixed orders and order_details routes/endpoints

---
+++ backend/models/OrderDetails.js
@@ -0,0 +1,48 @@
+import { getDB } from "../config/db.js";
+import { ObjectId } from "mongodb";
+
+export class Order {
+  static collection() {
+    return getDB().collection("orders");
+  }
+
+  static toObjectId(id) {
+    return typeof id === "string" ? new ObjectId(id) : id;
+  }
+
+  static async create(orderData) {
+    const order = {
+      ...orderData,
+      order_date: new Date(),
+      updated_at: new Date(),
+    };
+
+    return await this.collection().insertOne(order);
+  }
+
+  static async getById(id) {
+    return await this.collection().findOne({ _id: this.toObjectId(id) });
+  }
+
+  static async update(id, updates) {
+    updates.updated_at = new Date();
+    return await this.collection().updateOne(
+      { _id: this.toObjectId(id) },
+      { $set: updates }
+    );
+  }
+
+  static async delete(id) {
+    return await this.collection().deleteOne({ _id: this.toObjectId(id) });
+  }
+
+  static async getAll() {
+    return await this.collection().find().toArray();
+  }
+
+  static async findByCustomerId(customer_id) {
+    return await this.collection()
+      .find({ customer_id: this.toObjectId(customer_id) })
+      .toArray();
+  }
+}
